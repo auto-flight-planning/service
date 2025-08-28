2a8342d4b03812392e02bc5a3f7b649cb562fd26
Soru <chansolchoe9216@gmail.com>
2025-08-29 05:42:57 +0900

    [ui] analytics input main page

--- src/app/(web)/plan/[planId]/input/analytics/page.tsx
+++ src/app/(web)/plan/[planId]/input/analytics/page.tsx
@@ -1,12 +1,22 @@
 import InputHeader from "../_components/inputHeader";
+import AnalyticsInputContent from "./_components/analyticsInputContent";
 
-export default function AnalyticsInputPage() {
+interface AnalyticsPageProps {
+  params: Promise<{
+    planId: string;
+  }>;
+}
+
+export default async function AnalyticsPage({ params }: AnalyticsPageProps) {
+  const { planId } = await params;
+
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-8">
       <InputHeader
         title="運航日程企画のための分析データ"
         description="外部データ分析協力会社にデータを依頼"
       />
+      <AnalyticsInputContent planId={planId} />
     </div>
   );
 }
