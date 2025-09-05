f1ac072a44257c13853b6376b7808bc420bc149a
Soru <chansolchoe9216@gmail.com>
2025-09-06 03:31:16 +0900

    [feat] loadJSandFunc (with test)

--- src/features/plan/result/server/service.ts
+++ src/features/plan/result/server/service.ts
@@ -1,16 +1,19 @@
 import { loadCSV } from "@/server/lib/loader";
+import { loadJSAndGetFunc } from "@/server/lib/loader/loadJS";
 
 export class PlanResultService {
   async createPlanResult(planId: string) {
     // 1. input status の確認 (入力済みだと仮定する)
     // 2. input data の取得 (ダミーデータを使用する)
     // 3. 結果算出
-    const temp = await loadCSV(
+    const temp1 = await loadCSV(
       "candidate_flights",
       "domestic/41962055-679d-451c-9158-7c36e8d275c1/domestic.csv"
     );
-    console.log(temp);
+    const temp2 = await loadJSAndGetFunc(
+      "round_trip_normalization", // TODO: -> normalizer
+      "41962055-679d-451c-9158-7c36e8d275c1/round_trip_priority_normalizer.ts"
+    );
+    console.log(temp2);
   }
 }
-
-// https://ysjpcqpcxtvaddfagtqw.supabase.co/storage/v1/object/sign/candidate_flights/domestic/41962055-679d-451c-9158-7c36e8d275c1/domestic.csv?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hY2M4YjU1OS01NWYxLTQzNWEtYjUxOS0zNTliMzgwNmFmMzQiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJjYW5kaWRhdGVfZmxpZ2h0cy9kb21lc3RpYy80MTk2MjA1NS02NzlkLTQ1MWMtOTE1OC03YzM2ZThkMjc1YzEvZG9tZXN0aWMuY3N2IiwiaWF0IjoxNzU3MDkyNzUyLCJleHAiOjE3NTc2OTc1NTJ9.cffmqMLqIGVGaysJ34v0i-AFFvPd9iKWl8Xx42xnydQ
