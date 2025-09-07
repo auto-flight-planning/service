ad6620372de454182023f67f282baaa88a43d1e3
Soru <chansolchoe9216@gmail.com>
2025-09-07 21:39:03 +0900

    [fix] Fix dynamic allocation error

--- src/app/api/plans/route.ts
+++ src/app/api/plans/route.ts
@@ -4,9 +4,14 @@ import { withHandler } from "@/server/lib";
 import planService from "@/features/plan/base/server/service";
 import { createPlanReqSchema } from "@/features/plan/base/server/schemas/req.schema";
 import { createPlanResSchema } from "@/features/plan/base/server/schemas/res.schema";
+import { dateToString } from "@/lib/utils";
 
 export const POST = withHandler(
-  async (request: NextRequest, user: User) => {
+  async (
+    request: NextRequest,
+    { params }: { params: Promise<any> },
+    user: User
+  ) => {
     const requestBody = await request.json();
     const validatedRequestBody = createPlanReqSchema.parse(requestBody);
     const { title, targetDate, participantDataList } = validatedRequestBody;
@@ -22,7 +27,8 @@ export const POST = withHandler(
       id: plan.id,
       creatorId: plan.creator_id,
       title: plan.title,
-      targetDate: plan.target_date,
+      targetDate: dateToString(plan.target_date),
+      status: plan.status,
       participantDataList: planParticipants.map((participant) => ({
         userId: participant.user_id,
         permission: participant.permission,
