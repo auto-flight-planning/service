5abd738a6b0c04ed6b165a7dde2d1d4b933c05ea
Soru <chansolchoe9216@gmail.com>
2025-09-08 20:16:09 +0900

    [chore] remove unnecessary export

--- src/features/plan/base/create/index.ts
+++ src/features/plan/base/create/index.ts
@@ -1,7 +1,1 @@
 export { default as CreatePlanModal } from "./widgets/modal";
-export { default as CreatePlanForm } from "./widgets/form";
-export { default as useCreatePlan } from "./hooks/useCreatePlan";
-export {
-  createPlanSchema,
-  type CreatePlanFormDataType,
-} from "./schemas/formSchema";
