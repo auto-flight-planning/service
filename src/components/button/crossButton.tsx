4ae17dd5ffe60aa234f908b121d1b4cc98d80ff2
Soru <chansolchoe9216@gmail.com>
2025-09-09 04:43:04 +0900

    [design] add size props in CrossButton

--- src/components/button/crossButton.tsx
+++ src/components/button/crossButton.tsx
@@ -1,15 +1,31 @@
 import { ButtonHTMLAttributes } from "react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
-interface CrossButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface CrossButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: "small" | "medium";
+}
 
-export default function CrossButton(props: CrossButtonProps) {
+export default function CrossButton({
+  size = "medium",
+  ...props
+}: CrossButtonProps) {
+  const sizeClasses = {
+    small: {
+      button: "w-5 h-5",
+      icon: "w-4 h-4",
+    },
+    medium: {
+      button: "w-8 h-8",
+      icon: "w-5 h-5",
+    },
+  };
+
   return (
     <button
-      className="bg-none border-none text-gray-500 cursor-pointer p-0 w-8 h-8 flex items-center justify-center rounded-full transition-all duration-200 hover:bg-gray-100 hover:text-gray-700 text-2xl"
+      className={`bg-none border-none text-gray-500 cursor-pointer p-0 flex items-center justify-center rounded-full transition-all duration-300 hover:bg-gray-100 hover:text-gray-700 active:scale-90 text-2xl ${sizeClasses[size].button}`}
       {...props}
     >
-      <XMarkIcon className="w-5 h-5" />
+      <XMarkIcon className={sizeClasses[size].icon} />
     </button>
   );
 }
