a68826f42b86ab4733a88056044107be378c3bc0
Soru <chansolchoe9216@gmail.com>
2025-09-09 11:10:21 +0900

    [chore] add NotFoundError

--- src/server/lib/index.ts
+++ src/server/lib/index.ts
@@ -1,4 +1,4 @@
-export { NotFoundError } from "./errors";
+export * from "./errors";
 export { default as withHandler } from "./withHandler";
 export { default as withErrorHandler } from "./withErrorHandler";
 export { default as checkRequestBody } from "./checkRequestBody";
