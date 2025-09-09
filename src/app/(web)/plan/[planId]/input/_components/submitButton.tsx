e32e4b38a0db46c6438cd30e370697b499ef7639
Soru <chansolchoe9216@gmail.com>
2025-09-09 11:09:39 +0900

    [chore] remove legacy code

--- src/app/(web)/plan/[planId]/input/_components/submitButton.tsx
+++ src/app/(web)/plan/[planId]/input/_components/submitButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SquareButton } from "@/components/button";
-import { useGetPlanInfo } from "@/features/plan/base/get/one/useGetPlanInfo";
+// import { useGetPlanInfo } from "@/features/plan/base/get/one/useGetPlanInfo";
 import { useRouter } from "next/navigation";
 
 interface SubmitButtonProps {
@@ -9,23 +9,24 @@ interface SubmitButtonProps {
 }
 
 export default function SubmitButton({ planId }: SubmitButtonProps) {
-  const { data, isPending } = useGetPlanInfo(planId);
-  const resultStatus = data?.status.result_status;
-  const router = useRouter();
+  // const { data, isPending } = useGetPlanInfo(planId);
+  return null;
+  // const resultStatus = data?.status.result_status;
+  // const router = useRouter();
 
-  return (
-    <div className="flex justify-end">
-      <SquareButton
-        text="結果確認"
-        bold
-        color="primary"
-        size="large"
-        disabled={isPending || resultStatus !== "generated"}
-        isPending={resultStatus === "pending"}
-        onClick={() => {
-          router.push(`/plan/${planId}/result`);
-        }}
-      />
-    </div>
-  );
+  // return (
+  //   <div className="flex justify-end">
+  //     <SquareButton
+  //       text="結果確認"
+  //       bold
+  //       color="primary"
+  //       size="large"
+  //       disabled={isPending || resultStatus !== "generated"}
+  //       isPending={resultStatus === "pending"}
+  //       onClick={() => {
+  //         router.push(`/plan/${planId}/result`);
+  //       }}
+  //     />
+  //   </div>
+  // );
 }
